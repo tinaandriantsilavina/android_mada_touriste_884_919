0b21fd14268d7b27e481e70e8138280f7f3837fc
tinaandriantsilavina <tinaandriantsilavina@gmail.com>
2023-07-26 22:53:47 +0300

    Login

---
+++ app/src/main/java/com/madatouriste/vue/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.madatouriste.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.madatouriste.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
