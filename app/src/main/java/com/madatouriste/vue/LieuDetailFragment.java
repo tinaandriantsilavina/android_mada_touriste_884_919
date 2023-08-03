51ff01027a7c5566b639b0837887b10057ec8547
tinaandriantsilavina <tinaandriantsilavina@gmail.com>
2023-08-03 21:57:39 +0300

    lieu detail

---
+++ app/src/main/java/com/madatouriste/vue/LieuDetailFragment.java
@@ -0,0 +1,141 @@
+package com.madatouriste.vue;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.madatouriste.R;
+import com.madatouriste.constant.ProvinceConstant;
+import com.madatouriste.modele.Lieu;
+import com.madatouriste.modele.Province;
+import com.madatouriste.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class LieuDetailFragment extends Fragment {
+
+    Lieu lieu;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View v = inflater.inflate(R.layout.fragment_lieu_detail, container, false);
+        if(getArguments()!=null && getArguments().getSerializable("lieu")!=null){
+            lieu = (Lieu) getArguments().getSerializable("lieu");
+            initProvince();
+            initEntete(v);
+            initDescription(v);
+            initMenu(v);
+        }else{
+            Toast.makeText(getActivity(), "Veullez selectionner un Lieu ", Toast.LENGTH_SHORT).show();
+        }
+        return v;
+    }
+
+
+
+    public void initProvince() {
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json2 = ProvinceConstant.propvince;
+//            province = objectMapper.readValue(json2, Province.class);
+        }catch (Exception ex){
+            Toast.makeText(getActivity(), "Erreur ", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void initEntete(View v){
+        LinearLayout layout = v.findViewById(R.id.fragment_province_detail_layout);
+//        layout.setBackground();
+        TextView titre = layout.findViewById(R.id.fragment_province_detail_titre);
+        titre.setText(lieu.getNom());
+    }
+    public void initDescription(View v){
+        TextView titre = v.findViewById(R.id.fragment_province_detail_description_Titre);
+        TextView text = v.findViewById(R.id.fragment_province_detail_description_text);
+//        titre.setText("ketrika titre");
+        text.setText(lieu.getDescription());
+    }
+
+    public void initLieu(View v){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Lieu 1");
+        list.add("Lieu 2");
+        list.add("Lieu 3");
+
+        LinearLayout linearLayout = v.findViewById(R.id.fragment_province_detail_lieu_linearLayout);
+
+        for (int i = 0; i < list.size(); i++) {
+            String element = list.get(i);
+            TextView textView = new TextView(getActivity());
+            textView.setText(element);
+            textView.setTextSize(14);
+            textView.setPadding(40, 20, 20, 20);
+            textView.setTextColor(Color.parseColor("#c30097"));
+//          textView.setBackgroundColor(Color.LTGRAY);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "Clic sur : " + element, Toast.LENGTH_SHORT).show();
+                }
+            });
+            linearLayout.addView(textView);
+        }
+    }
+
+    public void initMenu(View v){
+        Button btnLieu = v.findViewById(R.id.fragment_province_detail_menu_btnLieu);
+        btnLieu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Clic sur : lieu" , Toast.LENGTH_SHORT).show();
+                LieuListFragment lieuListFragment = new LieuListFragment();
+//                imageGallerieFragment.setImageObjects(province.getImages());
+                //imageGallerieFragment.setImageObjects(Utils.getImage());
+                Utils.fragmentNavig(getActivity(), lieuListFragment);
+            }
+        });
+
+        Button btnPhoto = v.findViewById(R.id.fragment_province_detail_menu_btnPhoto);
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageGallerieFragment imageGallerieFragment = new ImageGallerieFragment();
+                imageGallerieFragment.setImageObjects(lieu.getImages());
+                //imageGallerieFragment.setImageObjects(Utils.getImage());
+                Utils.fragmentNavig(getActivity(), imageGallerieFragment);
+                Toast.makeText(getActivity(), "Clic sur : Photo" , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnVideo = v.findViewById(R.id.fragment_province_detail_menu_btnVideo);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String, Province> map = new HashMap<>();
+//                Utils.ecouteMenu(getActivity(), btnVideo, VideoActivity.class, map);
+                VideoFragment video = new VideoFragment();
+                Utils.fragmentNavig(getActivity(), video);
+                Toast.makeText(getActivity(), "Clic sur : btnVideo" , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
